2ece2994a7ab70bed91aec546beee91c3301e205
Pa1 <Pa1@gmail.com>
2023-02-27 00:11:24 +0530

    Initial commit

---
+++ assets/projects.py
@@ -0,0 +1,55 @@
+projects={
+"project4_description" : '''
+Alaska’s employee management system and appraisal system is a migration project and was implemented using Django giving a modern look and rich experience for their employees.
+''' ,
+"project4_contribution" : """
+Developed backend modules using Python on Django Framework .
+Had exposure in creating templates and enabling login and logout functionality as well.
+Created important business rules that are helpful for the customers.
+Actively involved in working with CRUD operations.
+Identify and fix bottlenecks that may arise from inefficient code.
+Identify and fix software bugs.
+""",
+"project4_techstack":
+"Python, Django, Postgresql, PythonAnywhere, Git, Jenkins",
+"project3_description" : """
+Project aims at pulling out reports from production and identifying the worst revenue areas and best revenue areas(Monthly, Quarterly etc..) and to identify areas that are struggling to reach their break even and to project the pictorial representation to higher officials and make them have the informative decision
+""",
+"project3_contribution" : """
+Performed data analysis using Python and Pandas and used Plotly for the pictorial representation.
+Good hands on in identifying effective ways of exploring the available information and converting them to meaningful information. 
+Wrote python scripts to alert the managers in case any of the stations had fall in revenues by nearly 30% from previous month and also alerts higher management incase of high staff unavailability.
+
+""",
+"project3_techstack":
+"Python, Pandas, Plotly",
+"project2_description" : """
+The project is entirely about testing the BOS software’s functionality solely and to identify hidden issues in BOS by sending simulating source data using XML and to verify BOS response is as per requirements
+""",
+"project2_contribution" : """
+Wrote automation scripts using Python and automated around 120+ scripts covering the requirements mentioned in Back Office Segment(BOS) FSD.
+Configured and maintained Jenkins to implement the CI process and thus paved a way for a smooth execution process.
+Extensively worked with regular expressions, file handling, date classes and few other third party libraries.
+Involved in client status meetings and act as a bridge between offshore and Onshore teams and ensure the deliverables are on time.
+""",
+"project2_techstack":
+"Python and Functional testing",
+"project1_description" : """
+PTC refers to communication-based/processor-based train control technology that provides a system capable of reliably and functionally preventing train-to-train collisions, over speed derailments, incursions into established work zone limits, and the movement of a train through a main line switch in the improper position. PTC systems vary widely in complexity and sophistication based on the level of automation and functionality they implement, the system architecture used and the wayside system upon which they are based (e.g., non-signaled, block signal, cab signal, etc.)
+I-ETMS (Interoperable Electronic Train Management System) is one of the PTC types approved by FRA.
+
+""",
+"project1_contribution" : """
+Rigorously worked in testing the systems at Segment, LINN and LIEE.
+Very much involved in MDM testing in the lab and shared the results with the clients.
+Has become expert in UP BOS execution, stabilized its environment and played a pivotal role in evolving the quality of the product and guided the team during their execution.
+Provided environment support for the team and ensured smooth execution for them
+
+
+""",
+"project1_techstack":
+"Functional testing, Manual testing",
+
+
+
+}
---
+++ digital_resume.py
@@ -0,0 +1,189 @@
+from pathlib import Path
+from PIL import Image
+import streamlit as st
+import pandas as pd
+from assets.projects import projects
+
+# ------ Path Settings -------
+# getting the parent folder of digital_resume python file
+if "__file__" in locals():
+    current_dir = Path(__file__).parent
+else:
+    current_dir = Path.cwd()
+
+css_file = Path(current_dir, "styles", "main.css")
+
+# Resume to be modified
+resume_file = Path(current_dir, "assets", "Pavan_Python_Django_PTC_Latest_2822023.docx - Google Docs.pdf")
+profile_pic = Path(current_dir, "assets", "profile-picsmall2.png")
+
+
+# ------ General Settings ----------
+PAGE_TITLE = "Digital Resume | Pavan A"
+PAGE_ICON = "👋"
+NAME = "Pavan Kumar Andalavari"
+
+PATH = Path(current_dir, "assets", "job_profile.txt")
+
+SKILLS_PATH = Path(current_dir, "assets", "skills.txt")
+
+COMPANIES_LIST = Path(current_dir, "assets", "companies.csv")
+
+# PROJECTS_DATA = Path(current_dir, "assets", "projects.py")
+
+
+with open(PATH, "r") as f:
+        JOB_PROFILE = f.readlines()
+
+with open(SKILLS_PATH, "r") as sp:
+    SKILLS = sp.readlines()
+
+    with open(COMPANIES_LIST, "r") as cl:
+        COMPANY_PROFILE = cl.readlines()
+
+EMAIL = "pavankumar.andalavari@gmail.com"
+PHONE = "+91- 9398346627"
+SOCIAL_MEDIA_LINKS = {
+    "GitHub" : "https://github.com/Pa1-09/",
+
+    # link to be modified
+    "Linkedin" : "https://in.linkedin.com/"
+
+}
+
+# to be decided and added later on
+PROJECTS = {
+    # Project name : Description
+    "Weather_Predictor" : "https://github.com/Pa1-09/Weather_Forecast_Project",
+    "Order_Service": "https://github.com/Pa1-09/ServiceAppNag"
+
+}
+
+
+# Real work starts here
+st.set_page_config(page_title=PAGE_TITLE, page_icon=PAGE_ICON)
+
+# Load CSS, PDF, Profile Pic
+with open(css_file, "r") as css:
+    css_f = css.read()
+    st.markdown(f"<style>{css_f}</style>",unsafe_allow_html=True)
+
+with open(resume_file, "rb") as pdf:
+    pdf_content = pdf.read()
+
+profile_picture = Image.open(profile_pic)
+
+
+# Hero Section
+col1, col2 = st.columns(2, gap="small")
+with col1:
+    st.image(profile_picture, width=200)
+
+with col2:
+    st.subheader(NAME)
+    st.write("Python and Django Developer.")
+    st.write("Manual and Automation Tester.")
+
+st.write("---")
+
+col4, col5, col6 = st.columns([1,2,2], gap="small")
+with col4:
+    st.write(" Phone: :telephone_receiver:", PHONE)
+
+with col5:
+    st.write("Email: :email:", EMAIL)
+
+with col6:
+    st.download_button(
+        label=":point_right: Download Resume(PDF)",
+        data = pdf_content,
+        file_name=resume_file.name,
+    )
+
+st.write("---")
+
+# Displaying social media links
+all_social_links = st.columns(len(SOCIAL_MEDIA_LINKS))
+for index, (social_media_name, social_link) in enumerate(SOCIAL_MEDIA_LINKS.items()):
+    all_social_links[index].write(f"[{social_media_name}]({social_link})")
+st.write("---")
+
+# Displaying experience and qualifications
+
+st.subheader("Experience and Qualifications")
+for exp_profile in JOB_PROFILE:
+        st.write(f"- {exp_profile}")
+
+
+# Displaying skills
+st.write("#")
+st.subheader("Skills")
+
+for each_skill in SKILLS:
+        st.write(f"- {each_skill}")
+
+
+# companies
+st.subheader("Professional Work Experience ")
+df = pd.read_csv(COMPANIES_LIST)
+st.write((df.loc[:,: ]))
+
+
+
+# ---- work history ----- to be written here
+st.subheader("Work history")
+
+st.write("Project 4 Description:")
+st.write(projects["project4_description"])
+st.write("\n")
+st.write("Project 4  Contribution:")
+st.write(projects['project4_contribution'])
+st.write("\n")
+st.write("Project 4  TechStack:")
+st.write(projects['project4_techstack'])
+
+st.write(3*"\n")
+
+st.write("Project 3 Description:")
+st.write(projects["project3_description"])
+st.write("\n")
+st.write("Project 3  Contribution:")
+st.write(projects['project3_contribution'])
+st.write("\n")
+st.write("Project 3  TechStack:")
+st.write(projects['project3_techstack'])
+
+st.write(3*"\n")
+
+st.write("Project 2 Description:")
+st.write(projects["project2_description"])
+st.write("\n")
+st.write("Project 2  Contribution:")
+st.write(projects['project2_contribution'])
+st.write("\n")
+st.write("Project 2  TechStack:")
+st.write(projects['project2_techstack'])
+
+st.write(3*"\n")
+
+st.write("Project 1 Description: ")
+st.write(projects["project1_description"])
+st.write("\n")
+st.write("Project 1  Contribution:")
+st.write(projects['project1_contribution'])
+st.write("\n")
+st.write("Project 1  TechStack:")
+st.write(projects['project1_techstack'])
+
+
+
+
+# Projects and Acoomplishments
+st.subheader("Projects and Accomplishments")
+# Displaying social media links
+all_project_links = st.columns(len(PROJECTS))
+for index, (project_name, project_link) in enumerate(PROJECTS.items()):
+    all_project_links[index].write(f"[{project_name}]({project_link})")
+st.write("---")
+
+
